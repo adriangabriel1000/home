f94f68f246f6406a5f325073f4ee4ce6ff76bcc2
adriangabriel1000 <adriangabriel1000@gmail.com>
2022-12-27 17:06:20 +0200

    Initial Commit

---
+++ home/homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeApp.swift
+//  home
+//
+//  Created by Adrian Gabriel on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
